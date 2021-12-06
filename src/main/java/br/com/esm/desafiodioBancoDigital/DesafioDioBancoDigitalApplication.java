755974a7fb1351a0bc1176ef9710e847b0a22a7a
Emerson Mascarenhas <esmascarenhas12@gmail.com>
2021-12-06 08:19:18 -0300

    Projeto desafio bootcamp AmDocs Java Developer da Dio

---
+++ src/main/java/br/com/esm/desafiodioBancoDigital/DesafioDioBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.esm.desafiodioBancoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/esm/desafiodioBancoDigital/DesafioDioBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.esm.desafiodioBancoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
